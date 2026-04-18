9a34b698679e68f1e8ae715ec5d1312993f6e6d0
liuchenlu <huhappy318@gmail.com>
2026-04-18 21:18:22 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#0f1110',
+        mist: '#d7d3ca',
+        ash: '#7c847f',
+        celadon: '#8faba6',
+        cloud: '#f1eee7',
+        gold: '#c9b184',
+      },
+      fontFamily: {
+        sans: ['"Noto Serif SC"', '"Source Han Serif SC"', '"PingFang SC"', 'serif'],
+      },
+      letterSpacing: {
+        calm: '0.18em',
+      },
+      boxShadow: {
+        veil: '0 0 120px rgba(190, 208, 202, 0.05)',
+      },
+    },
+  },
+  plugins: [],
+}
